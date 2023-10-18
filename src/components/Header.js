803a8f975d8e9ebe1da8d99da090e60d1df3aae1
muruga21 <murugaperumal21062004@gmail.com>
2023-10-18 19:39:03 +0530

    added navbar

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const Header = () => {
+
+    const [islogin , setIsLogin] = useState(false);
+    const username = "muruga";
+
+  return (
+    <nav className='flex justify-between mr-10 ml-10 mt-5 text-[#333]'>
+        <logo className="font-bold text-2xl md:text-4xl">
+            BLOGHERE
+        </logo>
+        <nav-ele>
+            <ul className='flex text-xl font-medium justify-center item-center'>
+                <li className='pr-5 pl-5 hover:border-red-100 hover:border-b-2'>
+                    Write
+                </li>
+                <li className='pr-5 pl-5 hover:border-red-100 hover:border-b-2'>
+                    {
+                        islogin ?(
+                            {username}
+                        ):("login")
+                    }
+                </li>
+            </ul>
+        </nav-ele>
+    </nav>
+  )
+}
+
+export default Header
